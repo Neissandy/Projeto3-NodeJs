2fe9ae11baa2c814d8d0f9e217ef6a55a74b0af9
Neissandy <neisandy.silva@gmail.com>
2022-05-20 19:42:47 -0300

    Versão 2

--- produto.js
+++ produto.js
@@ -1,13 +1,23 @@
-const id = '001';
-const valor = '10';
-const quantidade = '3';
+// const id = '001';
+// const valor = '10';
+// const quantidade = '3';
+// const quantMin = '1';
+// const quantMax = '10'
 
-const quantMin = '1';
-const quantMax = '10'
 
-const estoqueMedio = (quantMin * quantMax)/2;
+    const id = document.getElementById("idProdt");
+    const valor = document.getElementById("valorProdt");
+    const quantidade = document.getElementById("quantProdt");
+    const quantMin = document.getElementById("quantMinProdt");
+    const quantMax = document.getElementById("quantMaxProdt");
 
-const valorTotal = valor * quantidade;
+    function resposta (){
+        const estoqueMedio = (quantMin * quantMax)/2;
+        const valorTotal = valor * quantidade;
 
-console.log("valor total: " + valorTotal + "\n" + "Código do produto: " + id);
-console.log("Estoque médio de peças: " + estoqueMedio );
+        let text = "valor total: " + valorTotal + "\n" + "Código do produto: " + id + "\n" + "Estoque médio de peças: " + estoqueMedio;
+    }
+    
+
+// console.log("valor total: " + valorTotal + "\n" + "Código do produto: " + id);
+// console.log("Estoque médio de peças: " + estoqueMedio );
