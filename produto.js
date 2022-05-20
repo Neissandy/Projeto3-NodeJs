cdedebe9cc5b2ae94b8a8c77df31d577c8c4957f
Neissandy <neisandy.silva@gmail.com>
2022-05-19 21:23:34 -0300

    Versão 1

---
+++ produto.js
@@ -0,0 +1,13 @@
+const id = '001';
+const valor = '10';
+const quantidade = '3';
+
+const quantMin = '1';
+const quantMax = '10'
+
+const estoqueMedio = (quantMin * quantMax)/2;
+
+const valorTotal = valor * quantidade;
+
+console.log("valor total: " + valorTotal + "\n" + "Código do produto: " + id);
+console.log("Estoque médio de peças: " + estoqueMedio );
