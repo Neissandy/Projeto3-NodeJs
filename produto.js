a5334200d4f2049303677088c36c2422bd0d1ce2
Neissandy <neisandy.silva@gmail.com>
2022-05-23 16:59:49 -0300

    Versão 4

--- produto.js
+++ produto.js
@@ -1,10 +1,3 @@
-// const id = '001';
-// const valor = '10';
-// const quantidade = '3';
-// const quantMin = '1';
-// const quantMax = '10'
-
-
 const id = document.getElementById("idProdt");
 const valor = document.getElementById("valorProdt");
 const quantidade = document.getElementById("quantProdt");
@@ -12,22 +5,43 @@ const quantMin = document.getElementById("quantMinProdt");
 const quantMax = document.getElementById("quantMaxProdt");
 const btnEnviar = document.getElementById("btn");
 
-    function resposta (){
-        const estoqueMedio = (parseFloat(quantMin.value) + parseFloat(quantMax.value))/2;
-        const valorTotal = parseFloat(valor.value) * parseFloat(quantidade.value);
+function resposta (){
 
+    //Valor total;
+    const valorTotal = parseFloat(valor.value) * parseFloat(quantidade.value);
+    console.log('Valor total: ' + valorTotal);
+
+    //ID: No mínimo 4 caracteres;
+    //DEU ERRADO
+    const tamanhoId = id.length;
+    if (tamanhoId >= 4){
         console.log('Código do Produto: ' + id.value);
-        console.log('Valor total: ' + valorTotal);
+    }else{
+        alert("ID precisa ter pelo menos quatro caracteres!")
+    }
+
+    //MÍNIMO E MÁXIMO: A quantidade míima de peças não pode ser maior que a quantidade máxima;
+    //DEU ERRADO
+    if(quantMin > quantMax){
+        alert('Quantidade não permtida!')
+    }else{
+        const estoqueMedio = (parseFloat(quantMin.value) + parseFloat(quantMax.value))/2;
         console.log('Estoque Médio: ' + estoqueMedio);
+    }
+
 }
 btnEnviar.onclick = resposta;
 
+// //VALOR: Apenas valores númericos;
 
-// console.log("valor total: " + valorTotal + "\n" + "Código do produto: " + id);
-// console.log("Estoque médio de peças: " + estoqueMedio );
-
-// if (id.lenght < 4){
-//     console.log("O ID precisa ter pelo menos quatro caracteres.")
-// }else{
-//     console.log('Código do Produto: ' + id.value);
-// }
+// function onlynumber(evt) {
+//     var theEvent = evt || window.event;
+//     var key = theEvent.keyCode || theEvent.which;
+//     key = String.fromCharCode( key );
+//     //var regex = /^[0-9.,]+$/;
+//     var regex = /^[0-9.]+$/;
+//     if( !regex.test(key) ) {
+//        theEvent.returnValue = false;
+//        if(theEvent.preventDefault) theEvent.preventDefault();
+//     }
+//  }
